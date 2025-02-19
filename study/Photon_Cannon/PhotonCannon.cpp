242364c03e01f7bdd1c4c23c6aa52da1a49358cc
Taebin Kim <76577776+KimTaebin-ai@users.noreply.github.com>
2025-02-20 01:33:36 +0900

    deep copy, shallow copy

--- study/Photon_Cannon/PhotonCannon.cpp
+++ study/Photon_Cannon/PhotonCannon.cpp
@@ -1,26 +1,44 @@
 #include "PhotonCannon.hpp"
 
 Photon_Cannon::Photon_Cannon(int x, int y) {
-    std::cout << "생성자 호출 !" << std::endl;
     hp = shield = 100;
     coord_x = x;
     coord_y = y;
     damage = 20;
+    name = NULL;
+}
+
+Photon_Cannon::Photon_Cannon(int x, int y, const char *cannon_name)
+{
+    hp = shield = 100;
+    coord_x = x;
+    coord_y = y;
+    damage = 20;
+    name = new char[strlen(cannon_name) + 1];
+    strcpy(name, cannon_name);
 }
 
 Photon_Cannon::Photon_Cannon(const Photon_Cannon &pc) {
-    std::cout << "복사 생성자 호출 !" << std::endl;
+    std::cout << "복사 생성자 호출! " << std::endl;
     hp = pc.hp;
     shield = pc.shield;
     coord_x = pc.coord_x;
     coord_y = pc.coord_y;
     damage = pc.damage;
+    name = new char[strlen(pc.name) + 1];
+    strcpy(name, pc.name);
+}
+
+Photon_Cannon::~Photon_Cannon() {
+    if (name)
+        delete[] name;
 }
 
 void Photon_Cannon::show_status() {
-    std::cout << "Photon Cannon " << std::endl;
+    std::cout << "Photon Cannon :: " << name << std::endl;
     std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
-                << std::endl;
+
+              << std::endl;
 
     std::cout << " HP : " << hp << std::endl;
 }
--- study/Photon_Cannon/main.cpp
+++ study/Photon_Cannon/main.cpp
@@ -1,9 +1,8 @@
 #include "PhotonCannon.hpp"
 
 int main() {
-    Photon_Cannon pc1(3, 3);
-    Photon_Cannon pc2(pc1);
-    Photon_Cannon pc3 = pc2;
+    Photon_Cannon pc1(3, 3, "Cannon");
+    Photon_Cannon pc2 = pc1;
     pc1.show_status();
     pc2.show_status();
 
