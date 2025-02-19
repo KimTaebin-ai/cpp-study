33d67c8d69c9e68dd9a9fc821b4aa5f05b84eb73
Taebin Kim <76577776+KimTaebin-ai@users.noreply.github.com>
2025-02-19 23:53:17 +0900

    destructor

--- Marine/Marine.hpp
+++ Marine/Marine.hpp
@@ -3,22 +3,23 @@
 
 #include <iostream>
 
-class Marine {
-    private:
-        int hp; // 체력
-        int coord_x, coord_y;
-        int damage;
-        bool is_dead;
-
-    public:
-        Marine();             // 기본생성자
-        Marine(int x, int y); // x,y 좌표에 마린 생성
-
-        int attack();                      // 데미지 리턴
-        void be_attacked(int damage_earn); // 입는 데미지
-        void move(int x, int y);           // 새로운 위치
-
-        void show_status(); // 상태값 확인
+class Marine
+{
+    int hp;               // 마린 체력
+    int coord_x, coord_y; // 마린 위치
+    int damage;           // 공격력
+    bool is_dead;
+    char *name; // 마린 이름
+public:
+    Marine();                                      // 기본 생성자
+    Marine(int x, int y, const char *marine_name); // 이름까지 지정
+    Marine(int x, int y);                          // x, y 좌표에 마린 생성
+    ~Marine();
+    
+    int attack();                      // 데미지를 리턴한다.
+    void be_attacked(int damage_earn); // 입는 데미지
+    void move(int x, int y);           // 새로운 위치
+    void show_status(); // 상태값 확인
 };
 
 #endif
--- Marine/main.cpp
+++ Marine/main.cpp
@@ -1,20 +1,23 @@
 #include "Marine.hpp"
 
 int main() {
-    Marine* marines[100];
+    Marine *marines[100];
 
-    marines[0] = new Marine(2, 3);
-    marines[1] = new Marine(3, 4);
-
+    marines[0] = new Marine(2, 3, "Marine 2");
+    marines[1] = new Marine(1, 5, "Marine 1");
+    
     marines[0]->show_status();
     marines[1]->show_status();
+    
     std::cout << std::endl
               << "마린 1 이 마린 2 를 공격! " << std::endl;
-    marines[1]->be_attacked(marines[0]->attack());
+    marines[0]->be_attacked(marines[1]->attack());
+    
     marines[0]->show_status();
     marines[1]->show_status();
+    
     delete marines[0];
     delete marines[1];
-
+    
     return 0;
 }
